093046655e760ab8bf761835ca1dfa99c22317ee
Mufidatun Nuha <mufidnuha@gmail.com>
2022-02-04 11:14:52 +0700

    modified library

--- dags/spotify_etl_airflow/data_model.py
+++ dags/spotify_etl_airflow/data_model.py
@@ -2,7 +2,7 @@ import pandas as pd
 import requests
 from datetime import datetime, date
 import datetime as dt
-from posgres_conn import get_engine_from_settings
+from spotify_etl_airflow.posgres_conn import get_engine_from_settings
 from pangres import upsert
 
 def create_date():
